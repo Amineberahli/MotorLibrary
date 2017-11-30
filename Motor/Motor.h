a131da2affce3419fc3f3ec58c46377aa10a4e67
salimkhazem <votre@email.com>
2017-12-01 00:41:17 +0100

    Add comment to the library and more description int the program

--- Motor/Motor.h
+++ Motor/Motor.h
@@ -3,6 +3,25 @@
 * Developed by Salim Khazem
 */
 
+/*
+    Public methods :
+
+    * Motor (pin1,pin2,pinP) // constructor , it is used to instantiate the class  and pin1, pin2 are pins OUTPUT of motor, and pinP is the PWM pin to control motor 
+
+    * begin()    // To assign the pins to OUTPUT
+
+    * forward(int vit)  // This method allows the motor to be driven in the forward direction according to the speed defined 
+        The parameter "vit" is defined in percentage(0-100) and mapped into pwm range(0-255)
+
+    * rearward(int vit)  // This method allows the motor to be driven in the rearward direction according to the speed defined 
+        The parameter "vit" is defined in percentage(0-100) and mapped into pwm range(0-255)
+
+    * turnOff()  // This method turns off the motor 
+
+*/
+
+
+
 #ifndef Motor_h
 #define Motor_h
 #include <Arduino.h>
@@ -10,7 +29,7 @@ class Motor
 {
 public:
   Motor(int pin1, int pin2, int pinP); // Constructor
-  void affectation(); // Affectation method (turn on OUTPUT the pins )
+  void begin(); // Affectation method (turn on OUTPUT the pins )
   void forward(int vit); //Method to run the motor forward direction
   void rearward(int vit);  // Method to drive the motor to the rearward direction
   void turnOff();         // method to turn off the motor
